b9088fc814706246ff5efe01b8addcdc9ad1d71a
褚童 <32087257+UESTC-ChuTong@users.noreply.github.com>
2018-04-09 17:19:28 +0800

    Update 2018_04_07_075902_create_tag_table.php

--- api/database/migrations/2018_04_07_075902_create_tag_table.php
+++ api/database/migrations/2018_04_07_075902_create_tag_table.php
@@ -14,7 +14,7 @@ class CreateTagTable extends Migration
     public function up()
     {
         Schema::create('tag', function (Blueprint $table) {
-            $table->increments('id')->unique();
+            $table->increments('id');
             $table->integer('name')->unique()->comment('标签名');
             $table->integer('count')->default(0)->comment('该类标签有多少书');
             $table->timestamps();
